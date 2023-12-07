e262819c766b1a1d72f1c2d58875e6aded515787
IskandarConqueror <126427286+IskandarConqueror@users.noreply.github.com>
2023-12-07 20:06:30 -0300

    ADD - Se agregan pruebas del menu de configuracion, pruebas de verificacion y modificaciones a los modelos, tambien se dejan agregados los grupos de usuarios definidos como Seller y Manager

--- gradein_services/models/gradein_equipment.py
+++ gradein_services/models/gradein_equipment.py
@@ -2,7 +2,7 @@ from odoo import fields, models, api
 
 class GradeInQuestion(models.Model):
     _name = 'gradein.equipment'
-    _descrition = 'Grade In Equipment' # con este tag lo identificamos con el views
+    _description = 'Grade In Equipment' # con este tag lo identificamos con el views
 
     name = fields.Char(string='Nombre')
     image = fields.Binary(string='Imagen', attachment=True)
