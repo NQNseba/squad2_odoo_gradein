6371d32e82ee2b2b0eca4233215a2eaf4d82be25
IskandarConqueror <126427286+IskandarConqueror@users.noreply.github.com>
2023-12-12 17:45:08 -0300

    ADD - Se agrega al modulo su correspondiente traduccion, tambien se informa que ha sido cambiado el idioma a ingles en la aplicacion completamente.

--- gradein_services/models/gradein_equipment.py
+++ gradein_services/models/gradein_equipment.py
@@ -4,9 +4,9 @@ class GradeInEquipment(models.Model):
     _name = 'gradein.equipment'
     _description = 'Grade In Equipment' # con este tag lo identificamos con el views
 
-    name = fields.Char(string='Nombre')
-    image = fields.Binary(string='Imagen', attachment=True)
-    description = fields.Text(string='Descripción')
-    active = fields.Boolean(string='Activo', default=True)
-    price = fields.Float(string='Precio')
+    name = fields.Char(string='Name')
+    image = fields.Binary(string='Image', attachment=True)
+    description = fields.Text(string='Description')
+    active = fields.Boolean(string='Active', default=True)
+    price = fields.Float(string='Price')
 
--- gradein_services/models/gradein_reject_motive.py
+++ gradein_services/models/gradein_reject_motive.py
@@ -4,5 +4,5 @@ class GradeInRejectMotive(models.Model):
     _name = 'gradein.reject.motive'
     _description = 'Grade In Reject Motive' # con este tag lo identificamos con el views
 
-    name = fields.Char(string='Nombre')
-    active = fields.Boolean(string='Activo', default=True)
+    name = fields.Char(string='Name')
+    active = fields.Boolean(string='Active', default=True)
