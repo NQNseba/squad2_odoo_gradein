307b3fdbbe1f62bc7383aed9bd16f4713528b0a3
IskandarConqueror <126427286+IskandarConqueror@users.noreply.github.com>
2023-12-11 22:01:29 -0300

    ADD - Se agregan lo solicitado en los tickets T1S2-147, donde se solicita que se agregue los modelos de Tipo de Equipo, el T1S2-148 que solicitaba sus vistas junto a los filtros de busqueda y grupos. Tambien se completa el T1S2-149, donde se agrega el Grade In Type of Equipment al menu

--- gradein_services/__manifest__.py
+++ gradein_services/__manifest__.py
@@ -9,6 +9,7 @@
         'security/res_groups.xml',
         'security/ir.model.access.csv',
         'views/gradein_equipment_views.xml',
+        'views/gradein_equipment_type_views.xml',
         'views/gradein_question_views.xml',
         'views/gradein_answer_views.xml',
         'views/gradein_image_views.xml',
